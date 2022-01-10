a990417f5f342317f882b721eee8b5363f8b6690
Atilio Barreda <neo@Atilios-MBP.fios-router.home>
2022-01-09 23:52:09 -0500

    init

---
+++ pages/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <main>
+    <pre>
+      <div v-for="(c,i) in characters" :key="i">{{c}}</div>
+    </pre>
+  </main>
+</template>
+
+<script>
+import { groq } from '@nuxtjs/sanity'
+
+export default {
+  async asyncData({ $sanity }) {
+    console.log()
+    const query = groq`*[]`
+    const characters = await $sanity.fetch(query)
+    return { characters }
+  },
+}
+</script>
